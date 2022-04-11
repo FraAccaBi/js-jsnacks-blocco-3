7ec161100173bc8f44e314235eae219103613424
FraAccaBi <francescahuda@gmail.com>
2022-04-11 15:44:29 +0200

    snack 2 initial commit

---
+++ snack-2/main.js
@@ -0,0 +1,18 @@
+/* Snack 2:
+A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+Es: ['grogu', 'MANDO', 'Luke'] =>  ['Grogu', 'Mando', 'Luke'] */
+
+let nomi = ['Fra', 'FRA', 'fra']
+
+for (let i = 0; i < nomi.length; i++) {
+    let nome = nomi[i];
+    let iniziale = nome.substring(0,1)
+    console.log(iniziale);
+    if (iniziale !== iniziale.toUpperCase()){
+        nome = `${iniziale.toUpperCase()}${nome.substring(1)}`
+        nomi[i] = nome
+        
+    }
+}
+
+
