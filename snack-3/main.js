22bea9d7393244d590b49d1f9015abe3080d9f91
FraAccaBi <francescahuda@gmail.com>
2022-04-11 15:58:25 +0200

    snack 3 complete

--- snack-3/main.js
+++ snack-3/main.js
@@ -8,3 +8,25 @@ Es:
   { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
 ]
 Crea un nuovo array con la lista dei mammiferi. */
+
+const animali = [
+  { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+  { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+  { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+  { nome: 'rana', famiglia: 'ranidae', classe: 'anfibi' },
+  { nome: 'gatto', famiglia: 'felidi', classe: 'mammiferi' },
+  { nome: 'lince', famiglia: 'felidi', classe: 'mammiferi' },
+  { nome: 'passero', famiglia: 'fasianidi', classe: 'uccelli' },
+  { nome: 'lupo', famiglia: 'canidi', classe: 'mammiferi' },
+  { nome: 'iena', famiglia: 'canidi', classe: 'mammiferi' },
+  { nome: 'pettirosso', famiglia: 'fasianidi', classe: 'uccelli' },
+]
+
+const animali_mammiferi = animali.filter((animali) => {
+  return animali.classe === 'mammiferi'
+})
+console.log(animali_mammiferi);
+
+/* const diesel = automobili.filter((automobili) => {
+    return automobili.alimentazione === 'diesel'
+}) */
