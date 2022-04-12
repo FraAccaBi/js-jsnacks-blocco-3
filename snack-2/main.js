9a3022c510ebca9553866f0de9b34308f03c619e
FraAccaBi <francescahuda@gmail.com>
2022-04-12 09:56:52 +0200

    snack 2 fix

--- snack-2/main.js
+++ snack-2/main.js
@@ -2,19 +2,14 @@
 A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
 Es: ['grogu', 'MANDO', 'Luke'] =>  ['Grogu', 'Mando', 'Luke'] */
 
-let nomi = ['Fra', 'FRA', 'fra']
+let nomi = ['Fra', 'FRA', 'fra', 'fRa']
 
 for (let i = 0; i < nomi.length; i++) {
     let nome = nomi[i];
+    nome = nome.toLowerCase();
     let iniziale = nome.substring(0,1)
-    console.log(iniziale);
-    if (iniziale !== iniziale.toUpperCase()){
-        nome = `${iniziale.toUpperCase()}${nome.substring(1)}`
-        nomi[i] = nome   
-    } else {
-        nome = `${iniziale}${nome.substring(1).toLowerCase()}`
-        nomi[i] = nome
-    }
+    iniziale = iniziale.toUpperCase()
+    nomi[i] = `${iniziale}${nome.substring(1)}`
 }
 console.log(nomi);
 
